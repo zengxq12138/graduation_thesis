cfaaa32927d5e85c8782a0f7874f69e7daca687c
zxq <3092891585@qq.com>
2026-03-08 21:45:04 +0800

    更正doc图片文档

--- llm_test.py
+++ llm_test.py
@@ -3,12 +3,13 @@ import os
 from openai import OpenAI
 
 client = OpenAI(
-    api_key=os.getenv("DMX"),
-    base_url="https://www.dmxapi.com/v1/"
+    # api_key=os.getenv("DMX"),
+    api_key="sk-E5wWzyK17gGOaTgKLnE4EfbJrsJwXcIk5EJv7CVjeBZYwcqM",
+    base_url="https://ai.aiistart.com/v1"
 )
 
 completion = client.chat.completions.create(
-    model="glm-4.7",
+    model="gpt-5.2",
     messages=[
         # {"role": "system", "content": "你是一个聪明且富有创造力的小说作家"},
         {"role": "user", "content": "你是谁"}
